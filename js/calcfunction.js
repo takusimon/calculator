c4d6be359011a39327f20be9b52c8d060e1025e5
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-04-10 22:25:36 -0500

    Core Functionality set
    calculatore can perform basic calucaltor functionality

--- js/calcfunction.js
+++ js/calcfunction.js
@@ -24,7 +24,7 @@ function operate(firstNum, secondNum, operator) {
 	} else if (operator === "-") {
 		result = substract(firstNum, secondNum);
 		return result;
-	} else if (operator === "*") {
+	} else if (operator === "X") {
 		result = multiply(firstNum, secondNum);
 		return result;
 	} else if (operator === "/") {
@@ -37,4 +37,32 @@ let firstNum,
 	secondNum,
 	operator;
 
+const calcDisplay = document.querySelector(".display");
+const calcNumbers = document.querySelectorAll(".number");
+const calcOperators = document.querySelectorAll(".operator");
+const calcEquals = document.querySelector("#equals-btn");
 
+calcNumbers.forEach(btn => {
+	btn.addEventListener("click", () => {
+		calcDisplay.textContent += btn.textContent;
+	})
+});
+
+calcOperators.forEach(btn => {
+	btn.addEventListener("click", () => {
+		if (calcDisplay.hasChildNodes() && firstNum === undefined){
+			firstNum = Number(calcDisplay.textContent);
+			operator = btn.textContent;
+			calcDisplay.replaceChildren();
+		}
+	})
+});
+
+calcEquals.addEventListener("click", () => {
+	if (calcDisplay.hasChildNodes() && secondNum === undefined) {
+		secondNum = Number(calcDisplay.textContent);
+		calcDisplay.replaceChildren();
+		const result = operate(firstNum, secondNum,operator);
+		calcDisplay.textContent = result;
+	}
+});
